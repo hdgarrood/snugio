1a5c6f0d8196489c4721ba9e9d6727befa8a2b58
Petar Radosevic <petar@wunki.org>
2012-08-20 23:42:10 +0200

    Fix `cabal-dev ghci` command.

--- src/Snugio/Core.hs
+++ src/Snugio/Core.hs
@@ -1,11 +1,13 @@
-module Snugio.Core () where
+module Snugio.Core
+  ( Resource(..)
+  , b13 ) where
 
 -- Resource containing the defaults when possible and only the type when a
 -- custom made function is needed for the Resource
 class Resource a where
     service_available :: a -> Bool
     service_available _ = True
-    
+
     known_methods :: a -> [String]
 
 -- For now, fake the Request and Response types, this will be replaced by Snap
@@ -25,26 +27,10 @@ type Flow = [(State, Bool)]
 
 -- Checks for the availability of the Resource
 b13 :: (Resource r) => r -> Request -> Response -> Flow -> Response
-b13 res req resp flow 
+b13 res req resp flow
     | service_available res == True = b12 res req resp $ (B13, True) : flow
     | otherwise = "Service is down!"
 
 -- Test if the requested method is already known
 b12 :: (Resource r) => r -> Request -> Response -> Flow -> Response
 b12 res req resp flow = "Service is up"
-
--- Let's put the above to the test
--- Create a new type of resource, for now, this is a blog post
-data Post = Post
-     { title :: String
-     , content :: String
-     } deriving (Show)
-
--- Make it a resource
-instance Resource Post where
-    service_available _ = True
-    known_methods _ = ["GET", "POST", "DELETE"]
-    
--- Check the simple flow
-post = Post "title" "content"
-t = b13 post "request" "response" []
